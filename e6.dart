367f5c85658ab41602950b5920ede083f041165a
trung87471vmu <125439729+trung87471vmu@users.noreply.github.com>
2023-02-15 14:52:07 +0700

    Create e6.dart

---
+++ e6.dart
@@ -0,0 +1,8 @@
+//  6 Write a program to print full name of a from first name and last name using user input.
+void e6(){ 
+  print("Enter your first name: ");
+  String firstName = stdin.readLineSync()!;
+  print("Enter your last name: ");
+  String lastName = stdin.readLineSync()!;
+  print("Your name is $firstName $lastName");
+}

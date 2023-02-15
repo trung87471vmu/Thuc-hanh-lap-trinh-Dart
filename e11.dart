da9f8b18cb06894531686ccdaaabf7854422778f
trung87471vmu <125439729+trung87471vmu@users.noreply.github.com>
2023-02-15 14:54:56 +0700

    Create e11.dart

---
+++ e11.dart
@@ -0,0 +1,5 @@
+double e11(double bilAmount, int numberOfPeople){
+  // 11 Suppose, you often go to restaurant with friends and you have to split amount of bill.
+  // Write a program to calculate split amount of bill. Formula= (total bill amount) / number of people
+  return bilAmount / numberOfPeople;
+}

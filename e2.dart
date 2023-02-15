f28d20d4bf7a54c162aa8a5e184b25a592905d24
trung87471vmu <125439729+trung87471vmu@users.noreply.github.com>
2023-02-15 14:57:32 +0700

    Update e2.dart

--- e2.dart
+++ e2.dart
@@ -1,8 +1,6 @@
-// 2 Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
-void e2(){ 
-  String a = 'Hello I am “John Doe”';
-  String b = 'Hello I’am “John Doe”';
+void e2(){ // 2 Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+  String a = 'Hello I am \“John Doe\“';
+  String b = 'Hello I\’am “John Doe\”';
   print(a);
   print(b);
-  print("$a $b");
 }

ef4e9a9a9105b49353ab3da7b1480535295cac16
trung87471vmu <125439729+trung87471vmu@users.noreply.github.com>
2023-02-15 14:48:52 +0700

    Create e2.dart

---
+++ e2.dart
@@ -0,0 +1,8 @@
+// 2 Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+void e2(){ 
+  String a = 'Hello I am “John Doe”';
+  String b = 'Hello I’am “John Doe”';
+  print(a);
+  print(b);
+  print("$a $b");
+}
